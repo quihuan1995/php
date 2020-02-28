43cf39b7d8f7eceace3f18fabcec0e73427301e2
quihuan1995 <60122256+quihuan1995@users.noreply.github.com>
2020-02-28 08:23:08 +0700

    Update database.php

--- admin/database.php
+++ admin/database.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','vietpro_mobile_shop');
+$conn = mysqli_connect('localhost','root','','mobile_shop');
 mysqli_query($conn, "set names 'utf8'");
 ?>
