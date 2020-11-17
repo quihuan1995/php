33aab849a608a5fe4f39430d0387e7a84cca7603
qui huan <you@example.com>
2020-11-17 16:11:33 +0700

    add config mysql

--- admin/database.php
+++ admin/database.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost','root','','mobile_shop');
+$conn = mysqli_connect('sql12.freemysqlhosting.net','sql12377059','BWcdx1LZyb','sql12377059');
 mysqli_query($conn, "set names 'utf8'");
 ?>
